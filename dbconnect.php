a26007502087a51abbd63904f8fdae74bef2532a
ToshihiroMasui <it-kyobashi108@dc-itex.com>
2022-08-16 15:23:11 +0900

    dbconnect.php　編集

--- dbconnect.php
+++ dbconnect.php
@@ -1,31 +1,35 @@
 <?php
-try {
-    $db = new PDO('mysql:dbname=d-career_calender;host=localhost;port=8889;charset=utf8',"root","root");
-} catch(PDOException $e) {
-    print('DB接続エラー：' . $e->getMessage());
-}
-// require_once './env.php'
-// function connect(){
 
-//     $host = DB_HOST;
-//     $db = DB_NAME;
-//     $user = DB_USER;
-//     $pass = DB_PASS;
-
-//     $dsn = "mysql:host=$host;dbname=$db;charset=utf8"
-
-//     try{
-//         $pdo = new PDO($dsn, $user, $pass[
-//             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-//             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-//         ]);
-//         echo "成功です。"
-//     } catch(PDOExeption $e){
-//         print('DB接続エラー:' . $e->getMessage());
-//         exit();
-//     }
+// try {
+//     $db = new PDO('mysql:dbname=d-career_calender;host=localhost;port=8889;charset=utf8',"root","root");
+// } catch(PDOException $e) {
+//     print('DB接続エラー：' . $e->getMessage());
 // }
 
-// echo connect();
+
+require_once 'env.php';
+ini_set('display_errors', true);
+function connect(){
+
+    $host = DB_HOST;
+    $db = DB_NAME;
+    $user = DB_USER;
+    $pass = DB_PASS;
+
+    $dsn = "mysql:host=$host;dbname=$db;charset=utf8";
+
+    try{
+        $pdo = new PDO($dsn, $user, $pass,[
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]);
+        echo "成功です。";
+    } catch(PDOExeption $e){
+        echo '失敗'.$e ->getMessage();
+    }
+        
+}
+
+echo connect();
 
 ?>
