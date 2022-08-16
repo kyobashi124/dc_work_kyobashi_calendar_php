8e1460c63365ff0f9f23cdefdc962ba5dcd42395
ToshihiroMasui <it-kyobashi108@dc-itex.com>
2022-08-16 14:12:37 +0900

    更新

--- dbconnect.php
+++ dbconnect.php
@@ -4,4 +4,28 @@ try {
 } catch(PDOException $e) {
     print('DB接続エラー：' . $e->getMessage());
 }
+// require_once './env.php'
+// function connect(){
+
+//     $host = DB_HOST;
+//     $db = DB_NAME;
+//     $user = DB_USER;
+//     $pass = DB_PASS;
+
+//     $dsn = "mysql:host=$host;dbname=$db;charset=utf8"
+
+//     try{
+//         $pdo = new PDO($dsn, $user, $pass[
+//             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+//             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+//         ]);
+//         echo "成功です。"
+//     } catch(PDOExeption $e){
+//         print('DB接続エラー:' . $e->getMessage());
+//         exit();
+//     }
+// }
+
+// echo connect();
+
 ?>
